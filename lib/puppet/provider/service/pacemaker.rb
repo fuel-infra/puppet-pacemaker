fuel-infra/puppet-pacemaker

commit fe2789477439f54a8adecf87483e537417e943c8

Service provider title fix
* Use title instead of name in the service provider
* Use name only to disable the basic service

lib/puppet/provider/service/pacemaker.rb
@@ -13,7 +13,7 @@ Puppet::Type.type(:service).provide(:pacemaker, :parent => Puppet::Provider::Pac
   # original name passed from the type
   # @return [String]
   def title
-    @resource[:name]
+    @resource.title
   end
 
   # primitive name with 'p_' added if needed
@@ -57,8 +57,10 @@ Puppet::Type.type(:service).provide(:pacemaker, :parent => Puppet::Provider::Pac
 
   # name of the basic service without 'p_' prefix
   # used to disable the basic service
+  # uses unmodified "name" property if provided
   # @return [String]
   def basic_service_name
+    return @resource[:name] unless @resource.title == @resource[:name]
     return @basic_service_name if @basic_service_name
     if name.start_with? 'p_'
       basic_service_name = name.gsub(/^p_/, '')

commit 54c3481ebebaafeee912cbffa3f50382fa5c0103

Fix name when name != title
When name != title, service provider is not able to find a proper resource
in CIB. This patch fixes the behaviour caused by
fe2789477439f54a8adecf87483e537417e943c8

Change-Id: I19570a40a585db6d27f955deeff48bc4a0dedc7e
Signed-off-by: Sergii Golovatiuk <sgolovatiuk@mirantis.com>

lib/puppet/provider/service/pacemaker.rb
@@ -26,6 +26,12 @@ Puppet::Type.type(:service).provide(:pacemaker, :parent => Puppet::Provider::Pac
       @name = primitive_name
       return @name
     end
+    primitive_name = resource[:name]
+    if primitive_exists? primitive_name
+      debug "Primitive with name '#{primitive_name}' was found in CIB"
+      @name = primitive_name
+      return @name
+    end
     primitive_name = "p_#{title}"
     if primitive_exists? primitive_name
       debug "Using '#{primitive_name}' name instead of '#{title}'"